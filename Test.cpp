5fef95d980b7ce62374f7313d79cedd835ba0dc8
Shani Hayik <57874274+ShaniHayik@users.noreply.github.com>
2020-04-16 14:15:27 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -72,7 +72,8 @@ TEST_CASE("Test Relation") {
 }
 
 
-TEST_CASE("Test Find and Remove") {
+
+TEST_CASE("Test Find") {
     family::Tree T ("Shani");
     T.addFather("Shani", "Moshe")
             .addMother("Shani", "Idit")
@@ -191,91 +192,87 @@ TEST_CASE("Test Find and Remove") {
             CHECK(T3.find("great-grandmother") == "Yehudit");
             CHECK(T3.find("great-grandfather") == "Lior");
             CHECK(T3.find("great-great-grandmother") == "Shirly");
-
-//    family::Tree T4 ("yarden");
-//    T4.addFather("yarden", "ilan")//best father ever
-//            .addMother("yarden", "riki")//mother
-//            .addFather("ilan", "refael")//grandfather
-//            .addMother("ilan", "lusi")//grandMother
-//            .addMother("lusi", "galit")//great-grandMother
-//            .addFather("lusi", "moshe")//great-grandfather
-//            .addFather("moshe","yossi")//great-great-grandfather
-//            .addFather("yossi","avraham")//great-great-great-grandfather
-//            .addMother("avraham","danit");//great-great-great-great-grandMother
-//
-//            CHECK(T4.find("me") == string("yarden"));
-//            CHECK(T4.find("mother") == string("riki"));
-//            CHECK(T4.find("father") == string("ilan"));
-//            CHECK(T4.find("grandmother") == string("lusi"));
-//            CHECK(T4.find("grandfather") == string("refael"));
-//            CHECK(T4.find("great-grandmother") == string("galit"));
-//            CHECK(T4.find("great-grandfather") == string("moshe"));
-//            CHECK(T4.find("great-grandfather") == string("ofir"));//throw error
-//           // CHECK(T4.find("uncle") == string("tamir"));//throw error
-//            CHECK(T4.find("great-great-great-great-grandmother") == string("danit"));
-//            CHECK(T4.find("great-great-great-grandfather") == string("avraham"));
-//
-//            T4.remove("avraham");//avraham and danit removed
-//            //CHECK(T4.find("great-great-great-grandfather") == string("avraham"));//throw error
-//            CHECK(T4.find("great-great-great-great-grandmother") == string("danit"));
-//            T4.addFather("yossi","avraham").addMother("avraham","danit");
-//            T4.remove("moshe");//moshe,yossi,avraham and danit removed
-//            //CHECK(T4.find("great-grandfather") == string("moshe"));//trow error
-//            T4.remove("yarden");
-//            CHECK(T4.find("me") == string("yarden"));//error
 }
 
+TEST_CASE("Test all functions") {
+    family::Tree T ("yarden");
+    T.addFather("yarden", "ilan")//best father ever
+            .addMother("yarden", "riki")//mother
+            .addFather("ilan", "refael")//grandfather
+            .addMother("ilan", "lusi")//grandMother
+            .addMother("refael", "galit")//great-grandMother
+            .addFather("refael", "moshe")//great-grandfather
+            .addFather("moshe","yossi")//great-great-grandfather
+            .addFather("yossi","avraham")//great-great-great-grandfather
+            .addMother("avraham","tami")//great-great-great-great-grandMother
+            .addFather("avraham","shalom");
 
+            CHECK(T.find("me") == string("yarden"));
+            CHECK(T.find("mother") == string("riki"));
+            CHECK(T.find("father") == string("ilan"));
+            CHECK(T.find("grandmother") == string("lusi"));
+            CHECK(T.find("grandfather") == string("refael"));
+            CHECK(T.find("great-grandmother") == string("galit"));
+            CHECK(T.find("great-grandfather") == string("moshe"));
+            //CHECK(T.find("great-grandRather") == string("ofir"));//throw error
+            //CHECK(T.find("uncle") == string("tamir"));//throw error
+            CHECK(T.find("great-great-great-great-grandmother") == string("tami"));
+            CHECK(T.find("great-great-great-grandfather") == string("avraham"));
+            T.remove("avraham");//avraham shalom and tami removed
+            //CHECK(T.find("great-great-great-grandfather") == string("avraham"));//throw error
+            CHECK(T.find("great-great-grandfather") == string("yossi"));
+            T.remove("moshe");//yossi and moshe removed
+            //CHECK(T.find("great-grandfather") == string("moshe"));//trow error
+            T.remove("yarden");
+            //CHECK(T.find("me") == string("yarden"));//error
 
-//TEST_CASE("Test all functions") {
-//    family::Tree T ("yarden");
-//    T.addFather("yarden", "ilan")//best father ever
-//            .addMother("yarden", "riki")//mother
-//            .addFather("ilan", "refael")//grandfather
-//            .addMother("ilan", "lusi")//grandMother
-//            .addMother("lusi", "galit")//great-grandMother
-//            .addFather("lusi", "moshe")//great-grandfather
-//            .addFather("moshe","yossi")//great-great-grandfather
-//            .addFather("yossi","avraham")//great-great-great-grandfather
-//            .addMother("avraham","danit")//great-great-great-great-grandMother
-//            .addMother("danit","tami");
-//
-//            CHECK(T.find("great-great-great-great-great-grandMother") == string("tami"));
-//            CHECK(T.relation("yarden") == string("me"));
-//            CHECK(T.relation("ofir") == string("unrelated"));
-//            CHECK(T.find("great-grandmother") == string("galit"));
-//            CHECK(T.relation("ilan") == "father");
-//            CHECK(T.find("great-grandfather") == string("moshe"));
-//            CHECK(T.relation("riki") == "mother");
-//            CHECK(T.relation("ilan") == "futher");//error
-//            CHECK(T.find("great-grandmother") == string("galit"));
-//            CHECK(T.find("great-grandfather") == string("moshe"));
-//            CHECK(T.find("great-great-great-great-grandMother") == string("danit"));
-//
-//            T.remove("tami");
-//            CHECK(T.relation("tami") == string("unrelated"));
-//            T.addMother("danit", "tami")
-//            .addMother("tami", "shani")
-//            .addFather("danit", "roni");
-//            CHECK(T.find("great-great-great-great-great-grandFather") == string("roni"));
-//            CHECK(T.find("great-great-great-great-great-great-grandMother") == string("shani"));
-//            CHECK(T.find("father") == string("ilan"));
-//            CHECK(T.find("grandmother") == string("lusi"));
-//            CHECK(T.find("grandfather") == string("refael"));
-//            CHECK(T.find("great-grandmother") == string("galit"));
-//            CHECK(T.find("great-grandfather") == string("moshe"));
-//            CHECK(T.find("great-grandFuther") == string("ilan"));//throw error
-//            CHECK(T.find("brother") == string("tamir"));//throw error
-//            CHECK(T.find("great-great-great-great-grandfMother") == string("danit"));
-//            CHECK(T.find("great-great-great-grandfather") == string("avraham"));
-//            T.remove("avraham");//avraham and danit removed
-//            CHECK(T.relation("avraham") == string("unrelated"));
-//            CHECK(T.relation("danit") == string("unrelated"));
-//            T.remove("refael");
-//            CHECK(T.relation("lusi") == string("grandfMother"));
-//            CHECK(T.relation("refael") == string("unrelated"));
-//            T.addFather("ilan","refael");
-//            CHECK(T.relation("refael") == string("grandFather"));
-//
-//}
+    //recreate yarden's tree
+    family::Tree T2 ("yarden");
+    T2.addFather("yarden", "ilan")//best father ever
+            .addMother("yarden", "riki")//mother
+            .addFather("ilan", "refael")//grandfather
+            .addMother("ilan", "lusi")//grandMother
+            .addMother("refael", "galit")//great-grandMother
+            .addFather("refael", "moshe")//great-grandfather
+            .addFather("moshe","yossi")//great-great-grandfather
+            .addFather("yossi","avraham")//great-great-great-grandfather
+            .addMother("avraham","tami")//great-great-great-great-grandMother
+            .addFather("avraham","shalom");
 
+            CHECK(T2.find("great-great-great-great-grandmother") == string("tami"));
+            CHECK(T2.relation("yarden") == string("me"));
+            CHECK(T2.relation("ofir") == string("unrelated"));
+            CHECK(T2.find("great-grandmother") == string("galit"));
+            CHECK(T2.relation("ilan") == string("father"));
+            CHECK(T2.find("great-grandfather") == string("moshe"));
+            CHECK(T2.relation("riki") == "mother");
+            //CHECK(T2.relation("ilan") == "futher");//error
+            CHECK(T2.find("great-grandmother") == string("galit"));
+            CHECK(T2.find("great-grandfather") == string("moshe"));
+            CHECK(T2.find("great-great-great-great-grandfather") == string("shalom"));
+
+            T2.remove("tami");
+            CHECK(T2.relation("tami") == string("unrelated"));
+            T2.addMother("shalom", "danit")
+            .addFather("shalom", "roni");
+            CHECK(T2.find("great-great-great-great-great-grandfather") == string("roni"));
+            CHECK(T2.find("great-great-great-great-great-grandmother") == string("danit"));
+            CHECK(T2.find("father") == string("ilan"));
+            CHECK(T2.find("grandmother") == string("lusi"));
+            CHECK(T2.find("grandfather") == string("refael"));
+            CHECK(T2.find("great-grandmother") == string("galit"));
+            CHECK(T2.find("great-grandfather") == string("moshe"));
+            //CHECK(T2.find("great-grandFuther") == string("ilan"));//throw error
+            //CHECK(T2.find("brother") == string("tamir"));//throw error
+            CHECK(T2.find("mother") == string("riki"));
+            CHECK(T2.find("great-great-great-grandfather") == string("avraham"));
+            T2.remove("avraham");
+            CHECK(T2.relation("avraham") == string("unrelated"));
+            CHECK(T2.relation("danit") == string("unrelated"));
+            T2.remove("refael");
+            CHECK(T2.relation("lusi") == string("grandmother"));
+            CHECK(T2.relation("refael") == string("unrelated"));
+            T2.addFather("ilan","refael");
+            CHECK(T2.relation("refael") == string("grandfather"));
+
+}
